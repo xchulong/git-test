0067359722e582f67a04fe950f9a7bc3224f17a7
chul <chul@qq.com>
2022-09-25 17:26:47 +0800

    main commit

--- src/main/java/cn/clcoder/git/HelloGit.java
+++ src/main/java/cn/clcoder/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("second git");
+        System.out.println("main");
     }
 }
