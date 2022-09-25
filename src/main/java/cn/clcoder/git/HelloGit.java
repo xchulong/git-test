2ce64d5fdac57c10e389d7782255ddc64f0ada3d
chul <chul@qq.com>
2022-09-25 17:21:27 +0800

    third commit

--- src/main/java/cn/clcoder/git/HelloGit.java
+++ src/main/java/cn/clcoder/git/HelloGit.java
@@ -3,5 +3,6 @@ package cn.clcoder.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("second git");
     }
 }
